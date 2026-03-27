5a9de7d58d71ec13f5dd3aaf35292264717c42fc
Adaamss <aademhamdi@gmail.com>
2026-03-27 15:54:59 +0100

    feat: add animal type

---
+++ animals.types.ts
@@ -0,0 +1,58 @@
+export type AnimalGroup = "mammal" | "bird" | "reptile" | "amphibian" | "fish";
+
+export type Habitat = "forest" | "desert" | "ocean" | "freshwater" | "sky";
+
+export type Diet = "carnivore" | "herbivore";
+
+export type Color = "black" | "white" | "brown" | "gray" | "red";
+
+export type Capabilities = {
+  canFly: boolean;
+  canSwim: boolean;
+  canRun: boolean;
+};
+
+export type SizeCategory = "tiny" | "small" | "medium" | "large" | "giant";
+
+export type SocialStructure = "solitary" | "pair";
+
+export type Animal = {
+  id: string;
+  commonName: string;
+  group: AnimalGroup;
+  colors: Color[];
+  habitats: Habitat[];
+  diet: Diet;
+  socialStructure: SocialStructure;
+  capabilities: Capabilities;
+};
+
+export const eagle: Animal = {
+  id: "bird1",
+  commonName: "Eagle",
+  group: "bird",
+  colors: ["white", "brown"],
+  habitats: ["forest", "freshwater"],
+  diet: "carnivore",
+  socialStructure: "pair",
+  capabilities: {
+    canFly: true,
+    canSwim: false,
+    canRun: false,
+  },
+};
+
+export const cheetah: Animal = {
+  id: "chetaah1",
+  commonName: "Cheetah",
+  group: "mammal",
+  colors: ["brown"],
+  habitats: ["desert"],
+  diet: "carnivore",
+  socialStructure: "solitary",
+  capabilities: {
+    canFly: false,
+    canSwim: false,
+    canRun: true,
+  },
+};
