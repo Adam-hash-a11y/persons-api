37d6966ba13a3673eba0c27f075b21e97efa0576
Adaamss <aademhamdi@gmail.com>
2026-04-08 16:01:59 +0100

    test: add test for filtyerById fucntionality

---
+++ tests/api/filterById.test.ts
@@ -0,0 +1,68 @@
+import request from "supertest";
+import { app } from "../../server";
+import { Server } from "node:http";
+
+describe("GET /:id", () => {
+  let server: Server;
+  beforeAll((done) => {
+    server = app.listen(3000);
+    done();
+  });
+
+  afterAll((done) => {
+    server.close();
+    done();
+  });
+  it("should return the body of the given id", async () => {
+    //Given
+
+    const userId = 11;
+
+    //When
+
+    const response = await request(app).get(`/${userId}`);
+
+    //Then
+
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual([
+      {
+        id: 11,
+        name: "Mia Taylor",
+        age: 26,
+        gender: "female",
+        type: "women",
+      },
+    ]);
+  });
+  it("should return an error (404) not found for non existing id", async () => {
+    //Given
+
+    const userId = 30;
+
+    //When
+
+    const response = await request(app).get(`/${userId}`);
+
+    //Then
+
+    expect(response.status).toBe(404);
+    expect(response.body).toStrictEqual({ error: "person is not found" });
+  });
+  it("should return an error (400) bad request if the id is a string ", async () => {
+    //Given
+
+    const userId = "abc";
+
+    //When
+
+    const response = await request(app).get(`/${userId}`);
+
+    //Then
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({
+      error: "invalid id, the id must a number",
+    });
+  });
+});
---
+++ tests/service/filterById.test.ts
@@ -0,0 +1,46 @@
+import { filterById } from "../../personServices";
+
+describe("test filterById function", () => {
+  test("should return the element that matches the given Id", () => {
+    //Given
+    const id = 12;
+
+    //When
+
+    const result = filterById(id);
+
+    //Then
+
+    expect(result).toStrictEqual([
+      {
+        age: 8,
+        gender: "male",
+        id: 12,
+        name: "Lucas Moore",
+        type: "kid",
+      },
+    ]);
+  });
+  test("should return an empty array when no element macthes the id", () => {
+    //Given
+    const id = 20;
+
+    //When
+
+    const result = filterById(id);
+
+    //Then
+
+    expect(result).toStrictEqual([]);
+  });
+  test("should return an empty array if id is negative", () => {
+    //Given
+    const id = -1;
+
+    //When
+    const result = filterById(id);
+
+    //Then
+    expect(result).toStrictEqual([]);
+  });
+});
