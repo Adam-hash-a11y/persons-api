aefaf5b249b67a60860f31b766c513c9e9840b8e
Adaamss <aademhamdi@gmail.com>
2026-04-22 12:50:53 +0100

    test: add person validator tests

---
+++ tests/validator/person.validator.test.ts
@@ -0,0 +1,24 @@
+import { isValidId } from "../../src/validator/person.validator";
+
+describe("test isValidId validator function", () => {
+  it("should return true if the given id is an integer", () => {
+    //Given
+    const id = 15;
+
+    //When
+    const result = isValidId(id);
+
+    //Then
+    expect(result).toBeTruthy();
+  });
+  it("should return false if the given id is a string", () => {
+    //Given
+    const id: any = "aa";
+
+    //When
+    const result = isValidId(id);
+
+    //Then
+    expect(result).toBeFalsy();
+  });
+});
