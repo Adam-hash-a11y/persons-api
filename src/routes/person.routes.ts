512a37708b1623b966e16b1e34f3599d2925a604
Adaamss <aademhamdi@gmail.com>
2026-04-24 15:25:05 +0100

    feat: add GET route for filtering persons by query params

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -3,10 +3,11 @@ import {
   findPersonById,
   addPersons,
   getStats,
+  getPersons,
 } from "../controller/person.controller";
 
 export const personRouter = express.Router();
-
+personRouter.get("/", getPersons);
 personRouter.get("/stats", getStats);
 personRouter.get("/:id", findPersonById);
 personRouter.post("/add-person", addPersons);
